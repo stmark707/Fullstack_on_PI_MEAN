66ddae2b7d724a1e1ae82e6c90561211b79df206
stmark707 <stmark707@gmail.com>
2022-02-07 19:10:00 -0800

    creating the express server object

--- server/index.js
+++ server/index.js
@@ -1,1 +1,3 @@
-const express = require('express');
+const express = require('express'); //the require function is a part of the nodes
+//standard library, it imports the module given as an argument and returns it
+const app = express(); //we create an instance of the server application
