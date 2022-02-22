1f37bc1df3ce1db9ca8ffd37fa1b4dbb0c4d6dce
stmark707 <stmark707@gmail.com>
2022-02-21 19:03:14 -0800

    Fixing syntax error in script

--- server/public/script.js
+++ server/public/script.js
@@ -4,12 +4,12 @@ console.log('Executing client side javascript...');
 //Fetch API to retrieve Data
 
 fetch('/temperature')
-.then(results =>)
+.then(results =>
 {
   return results.text();
 });
 
-.then(text =>)
+.then(text =>
 {
   const temperatureDisplay = document.getElementById('temperature-diplay');
   temperatureDisplay.innerHTML = text;
