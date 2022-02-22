54ef0ba6ff169958afd8c9a2bcdca3f700908f8f
stmark707 <stmark707@gmail.com>
2022-02-21 19:01:47 -0800

    Adding script to HTML

--- server/public/script.js
+++ server/public/script.js
@@ -1,1 +1,16 @@
 console.log('Executing client side javascript...');
+
+
+//Fetch API to retrieve Data
+
+fetch('/temperature')
+.then(results =>)
+{
+  return results.text();
+});
+
+.then(text =>)
+{
+  const temperatureDisplay = document.getElementById('temperature-diplay');
+  temperatureDisplay.innerHTML = text;
+});
